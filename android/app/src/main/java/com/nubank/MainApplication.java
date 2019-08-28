34b041afe441cb8f13704b7c7ba72b7c80e7d442
Alex Souza <alexnsouza6@hotmail.com>
2019-08-27 23:33:48 -0300

    :wrench: configuring webview natively

--- android/app/src/main/java/com/nubank/MainApplication.java
+++ android/app/src/main/java/com/nubank/MainApplication.java
@@ -7,6 +7,7 @@ import com.facebook.react.PackageList;
 import com.facebook.hermes.reactexecutor.HermesExecutorFactory;
 import com.facebook.react.bridge.JavaScriptExecutorFactory;
 import com.facebook.react.ReactApplication;
+import com.reactnativecommunity.webview.RNCWebViewPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
       List<ReactPackage> packages = new PackageList(this).getPackages();
       // Packages that cannot be autolinked yet can be added manually here, for example:
       // packages.add(new MyReactNativePackage());
+      packages.add(new RNCWebViewPackage());
       return packages;
     }
 
